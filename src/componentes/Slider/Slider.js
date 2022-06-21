6dfe147b09f43d0a088dcca16cf9dffdf7fc36ac
Sadnan-2019 <54290526+Sadnan-2019@users.noreply.github.com>
2022-06-22 03:52:58 +0600

    now done demo

--- src/componentes/Slider/Slider.js
+++ src/componentes/Slider/Slider.js
@@ -2,33 +2,24 @@ import React from 'react';
 
 const Slider = () => {
      return (
-          <div class="carousel w-full py-10">
-          <div id="slide1" class="carousel-item relative w-full">
-            <img src="https://api.lorem.space/image/car?w=800&h=200&hash=8B7BCDC2" class="w-full" />
-            <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide4" class="btn btn-circle">❮</a> 
-              <a href="#slide2" class="btn btn-circle">❯</a>
-            </div>
-          </div> 
-          <div id="slide2" class="carousel-item relative w-full">
-            <img src="https://api.lorem.space/image/car?w=800&h=200&hash=500B67FB" class="w-full" />
-            <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide1" class="btn btn-circle">❮</a> 
-              <a href="#slide3" class="btn btn-circle">❯</a>
-            </div>
-          </div> 
-          <div id="slide3" class="carousel-item relative w-full">
-            <img src="https://api.lorem.space/image/car?w=800&h=200&hash=A89D0DE6" class="w-full" />
-            <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide2" class="btn btn-circle">❮</a> 
-              <a href="#slide4" class="btn btn-circle">❯</a>
-            </div>
-          </div> 
-          <div id="slide4" class="carousel-item relative w-full">
-            <img src="https://api.lorem.space/image/car?w=800&h=200&hash=225E6693" class="w-full" />
-            <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-              <a href="#slide3" class="btn btn-circle">❮</a> 
-              <a href="#slide1" class="btn btn-circle">❯</a>
+          <div className="hero min-h-screen  bg-slate-300  ">
+          <div className="hero-content flex-col lg:flex-row-reverse">
+            <img alt=""  data-aos="zoom-in-up"
+            data-aos-offset="300"
+            data-aos-delay="5000"
+            data-aos-duration="2000"
+            data-aos-easing="ease-in-out"
+        
+              src="https://images.unsplash.com/photo-1496328488450-9c5c5d555148?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"
+              className=" max-w-sm   rounded-lg  "
+            />
+            <div className="p-5"   data-aos="fade-down"
+            data-aos-offset="300"
+            data-aos-delay="5000"
+            data-aos-duration="3000">
+              <h1 className="text-5xl font-bold">We Provide   Architech Design  for Clint Satisfection  </h1>
+             
+              <button className="btn btn-primary">Get Started</button>
             </div>
           </div>
         </div>
